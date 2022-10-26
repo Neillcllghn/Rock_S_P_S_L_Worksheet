9b2af9b2ffe38f4cf19515ea54d5d1976b07ae68
Neillcllghn <neillcllghn@outlook.com>
2022-10-26 15:46:16 +0000

    removing the alert and adding the hidden text the function to confirm win, loss, draw to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,43 +21,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function game(userChoice) { 
     if(userChoice === "paper" && computerChoice === "rock" || userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper" || userChoice === "rock" && computerChoice === "lizard" || userChoice === "lizard" && computerChoice === "spock" || userChoice === "spock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "lizard" || userChoice === "lizard" && computerChoice === "paper" || userChoice === "paper" && computerChoice === "spock" || userChoice === "spock" && computerChoice === "rock") {
-        alert("Congradulations....You Win!!!");
         winScore();
     } else if (userChoice === computerChoice) {
-        alert ("It is a draw!");
         drawScore();
     } else {
-        alert ("Sorry! You lost!");
         loseScore();
     }
-    function continuGame() {
-        i = Math.floor(Math.random() * 5);
-        computerChoice = choices[i];
-    }
-    setTimeout(continuGame);
-}
-
-
-function resultNotifcation() {
-    let messages = document.getElementById("result")
-
-
+    
 }
 
 
 function winScore () {
     let winPoints = parseInt(document.getElementById("win").innerText);
     document.getElementById("win").innerText = ++winPoints;
+    document.getElementById("result").innerHTML = "Congradulations....You Win!!!";
 }
 
 function drawScore () {
     let drawPoints = parseInt(document.getElementById("draw").innerText);
     document.getElementById("draw").innerText = ++drawPoints;
+    document.getElementById("result").innerHTML = "It is a draw!";
 }
 
 function loseScore () {
     let losePoints = parseInt(document.getElementById("lose").innerText);
     document.getElementById("lose").innerText = ++losePoints;
+    document.getElementById("result").innerHTML = "Sorry, You lost!";
 }
 
 
